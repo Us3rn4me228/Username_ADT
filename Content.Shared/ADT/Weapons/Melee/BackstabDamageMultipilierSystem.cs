3698cb4f2bb4541b666fe14340402e3c58b98270
Username228 <semkin14jason@gmail.com>
2025-08-06 17:29:29 +0300

    мердж

--- Content.Shared/ADT/Weapons/Melee/BackstabDamageMultipilierSystem.cs
+++ Content.Shared/ADT/Weapons/Melee/BackstabDamageMultipilierSystem.cs
@@ -1,5 +1,7 @@
 using Content.Shared.Damage;
 using Content.Shared.Damage.Components;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
 using Content.Shared.Weapons.Melee.Events;
 
 namespace Content.Shared.Weapons.Melee.Backstab;
@@ -17,10 +19,17 @@ public sealed class BackstabDamageMultipilierSystem : EntitySystem
         foreach (var damaged in args.HitEntities)
         {
             if (damaged == args.User) continue; // проверка на пользователя оружия
+            if (!TryComp(damaged, out DamageableComponent? dmgcmp))
+                return;
+            if (!TryComp(damaged, out MobStateComponent? crit))
+                return;
             var degrees = Transform(damaged).LocalRotation.Degrees - Transform(args.User).LocalRotation.Degrees;
-            if (degrees >= 300 || degrees <= 60 && degrees >= -30) // проверка  на градус, работает криво
+            if (dmgcmp.DamageContainerID == "Biological" && crit.CurrentState == MobState.Alive)  // Чтоб не бэкстабать мехов и боргов. Ну и в принципе всё что угодно в игре
             {
-                _damageable.TryChangeDamage(damaged, ent.Comp.BonusDamage, ent.Comp.IgnoreResists, origin:args.User);
+                if (degrees >= 300 || degrees <= 60 && degrees >= -30) // проверка  на градус, работает криво
+                {
+                    _damageable.TryChangeDamage(damaged, ent.Comp.BonusDamage, ent.Comp.IgnoreResists, origin: args.User);
+                }
             }
         }
     }
